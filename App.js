95ed6aef18f30dbcd2e9f5b9115ec2ed616448b6
Sampath Surineni <sampath.skt@gmail.com>
2019-06-27 20:59:17 +0530

    added react navigation

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+'use strict';
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
@@ -9,41 +10,35 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import {createStackNavigator,
+       createAppContainer} from 'react-navigation';
 
 type Props = {};
-export default class App extends Component<Props> {
+class RingtonePage extends Component<Props> {
+    static navigationOptions = {
+        title: 'Ringtone Randomizer'
+    };
+    
   render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
+      return <Text style={styles.description}>Search for houses to buy!</Text>;
   }
 }
 
+const RootStack = createStackNavigator({
+    Home: { screen: RingtonePage},
+});
+
+const App = createAppContainer(RootStack);
+
+export default App;
+
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
+  description: {
+    fontSize: 18,
     textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+    color: '#656565',
+    marginTop: 65,
   },
 });
+
